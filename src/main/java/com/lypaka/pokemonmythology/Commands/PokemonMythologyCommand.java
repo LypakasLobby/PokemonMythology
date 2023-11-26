1284da0145ceb5f8f903ab6d5804e1f2e447a17f
Lypaka <ryan96t@gmail.com>
2023-11-26 13:35:46 -0600

    broken suggestions provider

--- src/main/java/com/lypaka/pokemonmythology/Commands/PokemonMythologyCommand.java
+++ src/main/java/com/lypaka/pokemonmythology/Commands/PokemonMythologyCommand.java
@@ -38,8 +38,31 @@ public class PokemonMythologyCommand {
     public static void loadSuggestions() {
 
         List<String> mythicList = new ArrayList<>(ConfigGetters.displayTitles.keySet());
-        mythicList.addAll(ConfigGetters.customMythics.keySet());
-        MYTHICS = (context, builder) -> ISuggestionProvider.suggest(mythicList, builder);
+        System.out.println("default mythics size == " + ConfigGetters.displayTitles.keySet().size());
+        System.out.println("custom mythics size == " + ConfigGetters.customMythics.keySet().size());
+        int indexes = ConfigGetters.displayTitles.keySet().size() + ConfigGetters.customMythics.keySet().size();
+        System.out.println("amount of indexes == " + indexes);
+        String[] mythicOptions = new String[indexes];
+        int startingIndex = 0;
+        for (int i = 0; i < mythicList.size(); i++) {
+
+            System.out.println("setting index " + i + " on array to " + mythicList.get(i));
+            mythicOptions[i] = mythicList.get(i);
+            startingIndex++;
+
+        }
+        List<String> customMythics = new ArrayList<>(ConfigGetters.customMythics.keySet());
+        System.out.println("keySet == " + ConfigGetters.customMythics.keySet());
+        System.out.println("startingIndex == " + startingIndex + " at the start of adding custom mythics");
+        for (int i = 0; i < customMythics.size(); i++) {
+
+            mythicOptions[startingIndex] = customMythics.get(i);
+            System.out.println("setting index " + startingIndex + " on array to " + customMythics.get(i) + " at list index " + i);
+            startingIndex++;
+
+        }
+        MYTHICS = (context, builder) -> ISuggestionProvider.suggest(mythicOptions, builder);
+        System.out.println("options == " + Arrays.toString(mythicOptions));
 
     }
 
